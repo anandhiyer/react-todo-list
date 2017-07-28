99422882d189a19b4b15daf4a34cc321dc1e6f78
anandhiyer <anandhariharaniyer@gmail.com>
2017-07-28 11:59:58 -0400

    part3 react to do list
    part3 react example complete

--- webpack.config.js
+++ webpack.config.js
@@ -1,1 +1,21 @@
-
+module.exports = {
+  entry: ['./client/client.js'],
+  devtool:'inline-source-map',
+  output: {
+      path: './dist',
+      filename: 'bundle.js',
+      publicPath: '/'
+  },
+    module:  {
+        loaders: [
+          {
+            test: /\.js$/,
+            loader: 'babel-loader',
+            exclude: /node_modules/,
+            query: {
+                presets: ["es2015", "react"]
+            }
+          }
+        ]
+    }
+}
